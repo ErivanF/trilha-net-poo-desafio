f1818349b114576dabced50dc39f07876442a453
ErivanF <erivan.c39@gmail.com>
2025-03-19 14:28:39 -0300

    feat:implementar classe smartphone

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,11 +3,16 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo {get;}
+        private string IMEI {get;}
+        private int Memoria {get;}
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string IMEI, int Memoria)
         {
             Numero = numero;
+            Modelo = modelo;
+            IMEI = IMEI;
+            Memoria = Memoria;
             // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
